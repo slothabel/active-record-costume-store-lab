77c482d9ee848139527f332759361c50f8d0df72
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-22 17:36:30 -0400

    commit tree 268fb0e0388756eafd70205502c6601a8aa28e96

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
-class CreateHauntedHouse < ActiveRecord::Migration[5.2]
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
 
     def change
         create_table :haunted_houses do |t|
